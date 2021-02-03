65107728fe9cea1f70a0fde9b8bdf2177e2e344d
Anikait <50035624+anikait1@users.noreply.github.com>
2021-02-03 17:51:23 +0530

    App header

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "#1DA1F2",
+        color: "#F5F8FA",
+        textAlign: "center",
+        fontSize: "2rem",
+        fontWeight: "500",
+        padding: "1rem",
+        boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px",
+      }}
+    >
+      Twitter Thread Generator
+    </header>
+  );
+}
+
+export default Header;
