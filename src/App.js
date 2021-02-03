bf538f0a947afd134c2fdbb605bbae75c81cead8
Anikait <50035624+anikait1@users.noreply.github.com>
2021-02-03 17:12:27 +0530

    Basic design of app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,50 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>App</div>
+  const [text, setText] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    let _tweets = [];
+    let tweet = "";
+
+    text
+      .replaceAll("[...]", " [...] ")
+      .split(" ")
+      .filter((word) => word !== "")
+      .forEach((word) => {
+        if (word === "[...]") {
+          _tweets.push(tweet.trim());
+          tweet = "";
+        } else {
+          if (word.length + tweet.length > 280 && tweet !== "") {
+            _tweets.push(tweet.trim());
+            tweet = "";
+          }
+
+          tweet += " " + word;
+        }
+      });
+
+    _tweets.push(tweet.trim());
+    setTweets(_tweets);
+  }, [text]);
+
+  return (
+    <div>
+      <h4>{tweets.reduce((currLen, tweet) => currLen + tweet.length, 0)}</h4>
+      <textarea
+        value={text}
+        onChange={(event) => setText(event.currentTarget.value)}
+      />
+      <button onClick={() => setText((t) => t + "[...]")}>Split</button>
+      <ol>
+        {tweets.map((tweet) => (
+          <li>{`${tweet.length}: ${tweet}`}</li>
+        ))}
+      </ol>
+    </div>
+  );
 }
+
+export default App;
